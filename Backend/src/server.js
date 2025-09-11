68bb24230ecd3e8640185b819371e021a4702eca
AdityaTeachZone-3D <adityarvindapatil@gmail.com>
2025-09-11 15:55:53 +0530

    chore: update .gitignore and fix main entry in package.json; enhance server.js with improved CORS handling and add README documentation

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 const app = express();
 import "dotenv/config";
-import cors from 'cors'
+import cors from "cors";
 // auth route
 import authRoute from "./routes/auth.route.js";
 // user route
 import userRoutes from "./routes/user.route.js";
 // chat route
-import chatRoutes from "./routes/chat.route.js"
-// Db connection 
+import chatRoutes from "./routes/chat.route.js";
+// Db connection
 import { ConnectToDB } from "./lib/db.config.js";
 import cookieParser from "cookie-parser";
 
@@ -18,35 +18,26 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-const allowedOrigin = process.env.CORS_ORIGIN.split(',');
+const allowedOrigin = process.env.CORS_ORIGIN;
 
 // set limit to request to apis
 
-
-app.use(cors({
-    origin : function (origin, callback) {
-      // If there's no origin (e.g., for localhost, or some API call), allow it
-      if (!origin || allowedOrigin.indexOf(origin) !== -1) {
-        callback(null, true);  // Allow the request
-      } else {
-        callback(new Error('Somethig went wrong...'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials : true,
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
 app.use(cookieParser());
 app.use(express.json());
 
-
 // Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
 
 app.listen(port, () => {
   console.log(`Port ruinng on ${port}`);
