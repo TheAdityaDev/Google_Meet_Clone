6f698cf89a08f118de563466741ac03ba8fab3b2
AdityaTeachZone-3D <adityarvindapatil@gmail.com>
2026-06-01 16:31:11 +0530

    fix: update cookie settings for production environment in auth controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export async function signup(req, res) {
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -93,7 +93,7 @@ export async function login(req, res) {
     res.cookie("token", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
     });
 
@@ -105,7 +105,10 @@ export async function login(req, res) {
 
 export function logout(req, res) {
   try {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
     res.status(200).json({ message: "Logout successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
